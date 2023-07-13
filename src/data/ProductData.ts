94c57749e8e104bd0321ac7e708a67ec91d60461
Brady <brady.savarie@hotmail.com>
2023-07-13 00:09:25 -0400

    Create dummy data and render filtered products

---
+++ src/data/ProductData.ts
@@ -0,0 +1,32 @@
+type Product = {
+  id: number;
+  name: string;
+  manufacturer: string;
+  category: string;
+};
+
+const ProductData: Product[] = [
+  { id: 1, name: 'EP Booster', manufacturer: 'Xotic', category: 'Boost' },
+  {
+    id: 2,
+    name: 'BD-2 Blues Driver',
+    manufacturer: 'Boss',
+    category: 'Overdrive',
+  },
+  { id: 3, name: 'RAT 2', manufacturer: 'ProCo', category: 'Distortion' },
+  {
+    id: 4,
+    name: 'Big Muff',
+    manufacturer: 'Electro-Harmonix',
+    category: 'Fuzz',
+  },
+  { id: 5, name: 'DD3', manufacturer: 'Boss', category: 'Delay' },
+  {
+    id: 6,
+    name: 'Afterneath',
+    manufacturer: 'Earthquaker Devices',
+    category: 'Reverb',
+  },
+];
+
+export default ProductData;
--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,10 +1,24 @@
+import { useParams } from 'react-router-dom';
 import Categories from '../components/Categories';
+import ProductData from '../data/ProductData';
 
 function Shop() {
+  const { category } = useParams();
+  let products = ProductData;
+
+  if (category !== 'All') {
+    products = ProductData.filter((product) => product.category === category);
+  }
+
   return (
     <>
       <h1>Shop Page</h1>
       <Categories />
+      <div>
+        {products.map((product) => {
+          return <li key={product.id}>{product.name}</li>;
+        })}
+      </div>
     </>
   );
 }
