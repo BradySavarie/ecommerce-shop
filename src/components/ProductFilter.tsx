852451eff3c2b5f68d35e567a0a9f03fe87e8399
Brady <brady.savarie@hotmail.com>
2023-07-25 01:08:19 -0400

    Complete brand filtering logic

--- src/components/ProductFilter.tsx
+++ src/components/ProductFilter.tsx
@@ -1,13 +1,58 @@
-import { Divider, Typography } from '@mui/material';
+import { Container, Divider, Typography } from '@mui/material';
 import { StyledProductFilter } from './styles/ProductFilter.styled';
+import ProductData from '../data/ProductData';
+import { ChangeEvent } from 'react';
 
-export function ProductFilter() {
+type ProductFilterProps = {
+  filterByManufacturer: (manufacturer: String, isChecked: Boolean) => void;
+};
+
+export function ProductFilter(props: ProductFilterProps) {
+  const { filterByManufacturer } = props;
+  const manufacturers: string[] = [];
+
+  ProductData.forEach((product) => {
+    if (!manufacturers.includes(product.manufacturer)) {
+      manufacturers.push(product.manufacturer);
+    }
+  });
+
+  function handleCheckboxChange(e: ChangeEvent<HTMLInputElement>) {
+    const { name, checked } = e.target;
+    filterByManufacturer(name, checked);
+  }
+  /* 
+  create method that filters products by brand (lift state up). receives brand as input, no output
+  for each manufacturer, create a checkbox element with a label that matches the manufacturer. When checkbox is clicked the filter method is called with the value of the brand that was clicked.
+  */
+
   return (
     <StyledProductFilter>
       <Typography variant="h6" fontWeight="600">
         Shop By Brand
       </Typography>
-      <Divider />
+      <Divider sx={{ marginY: (theme) => theme.spacing(1) }} />
+      {manufacturers.map((manufacturer) => (
+        <Container
+          disableGutters
+          key={manufacturer}
+          sx={{
+            display: 'flex',
+            justifyContent: 'flex-start',
+            gap: (theme) => theme.spacing(3),
+            marginY: (theme) => theme.spacing(1),
+          }}
+        >
+          <input
+            type="checkbox"
+            name={manufacturer}
+            onChange={handleCheckboxChange}
+          />
+          <Typography variant="body2" color={'GrayText'}>
+            {manufacturer}
+          </Typography>
+        </Container>
+      ))}
     </StyledProductFilter>
   );
 }
--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,16 +1,59 @@
 import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import ProductData from '../data/ProductData';
 import ProductCard from '../components/ProductCard';
 import { Grid, Typography, Container, Box } from '@mui/material';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import { ProductFilter } from '../components/ProductFilter';
+import { Product } from '../data/ProductData';
 
 function Shop() {
   const { category } = useParams();
-  let products = ProductData;
+  const [currentProducts, setCurrentProducts] = useState(ProductData);
+  const [filteredProducts, setFilteredProducts] = useState(currentProducts);
+  const [selectedManufacturers, setSelectedManufacturers] = useState<String[]>(
+    []
+  );
 
-  if (category !== 'All') {
-    products = ProductData.filter((product) => product.category === category);
+  useEffect(() => {
+    if (category !== 'All') {
+      setCurrentProducts(
+        ProductData.filter((product) => product.category === category)
+      );
+    } else {
+      setCurrentProducts(ProductData);
+    }
+  }, [category]);
+
+  useEffect(() => {
+    // if no filters are applied, show all current products
+    filterProducts();
+    // when selectedManufacturers or currentProducts change, update filtered products. render only filtered products.
+  }, [selectedManufacturers, currentProducts]);
+
+  function filterProducts() {
+    // If no filters are applied, show all current products
+    if (selectedManufacturers.length === 0) {
+      setFilteredProducts(currentProducts);
+    } else {
+      // Filter products based on selected manufacturers
+      const filteredProducts = currentProducts.filter((product) =>
+        selectedManufacturers.includes(product.manufacturer)
+      );
+      setFilteredProducts(filteredProducts);
+    }
+  }
+
+  function filterByManufacturer(manufacturer: String, isChecked: Boolean) {
+    if (isChecked) {
+      if (!selectedManufacturers.includes(manufacturer)) {
+        setSelectedManufacturers((prev) => [...prev, manufacturer]);
+      }
+    } else {
+      setSelectedManufacturers((prev) =>
+        prev.filter((item) => item !== manufacturer)
+      );
+    }
   }
 
   return (
@@ -23,14 +66,14 @@ function Shop() {
         </Typography>
       </Container>
       <Container sx={{ mb: 4, display: 'flex' }}>
-        <ProductFilter />
+        <ProductFilter filterByManufacturer={filterByManufacturer} />
         <Grid
           container
           sx={{
             justifyContent: 'center',
           }}
         >
-          {products.map((product) => {
+          {filteredProducts.map((product: Product) => {
             return (
               <Grid item key={product.id}>
                 <Box p={1}>
