3a734db7a718a68ed80c033500f146e487ec9120
Brady <brady.savarie@hotmail.com>
2023-07-26 00:22:00 -0400

    Update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -23,4 +23,9 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: ['./src/setupTests.ts'],
   },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
 });
