757906a0fbe7b5321302c66cdf0f775c3717f1af
Brady <brady.savarie@hotmail.com>
2023-07-26 00:33:42 -0400

    Fix background image url

--- src/pages/styles/Home.styled.tsx
+++ src/pages/styles/Home.styled.tsx
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
+import backgroundImg from '../../assets/background.jpg';
 
 const StyledHome = styled.div`
-  background-image: url('/src/assets/background.jpg');
+  background-image: url(${backgroundImg});
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
